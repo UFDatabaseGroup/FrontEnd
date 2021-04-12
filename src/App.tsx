a48f16f021bc9345ce1983d0bc7839782cf8eb4d
Scott Lagler <laglerscott@gmail.com>
2021-04-12 17:02:27 -0500

    Add chart.js dependency and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, Link, BrowserRouter} from 'react-router-dom'
 
 import Login from "./components/login.component";
 import SignUp from "./components/signup.componenet";
@@ -9,15 +9,14 @@ import QuerySelection from "./components/QuerySelection.component";
 import DataDisplay from "./components/data-display.component";
 
 function App() {
-  // const [token, setToken] = useState();
-  const token = 5;
+  const [token, setToken] = useState();
 
   if(!token) {
-    return <Login setToken />
+    return <Login setToken={setToken}/>
   }
 
   return (
-      <Router>
+      <BrowserRouter>
         <div className="App">
           <nav className="navbar navbar-expand-lg navbar-light fixed-top">
             <div className="container">
@@ -36,13 +35,18 @@ function App() {
           </nav>
 
           <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-            <Route path="/query-select" component={QuerySelection} />
-            <Route path="/trend-query" component={DataDisplay} />
+            <Route path='/'>
+              <QuerySelection/>
+            </Route>
+            <Route path="/sign-up">
+              <SignUp/>
+            </Route>
+            <Route path="/trend-query">
+              <DataDisplay/>
+            </Route>
           </Switch>
-        </div></Router>
+        </div>
+      </BrowserRouter>
   );
 }
 
--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,60 +1,55 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
 import axios from "axios";
 
-export default class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: null,
-            password: null
-        };
+async function loginUser(credentials) {
+    let response = await axios({
+        method: 'post',
+        url: 'http://localhost:3005/auth/login',
+        responseType: 'json'
+    })
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value});
-    }
-
-    async handleSubmit(event) {
-        alert(`Your favorite flavor is: ${this.state.username} and ${this.state.password} `);
-
-        const user = {
-            username: this.state.username,
-            passwd: this.state.password
-        }
-
-        await axios.post(`${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}/auth/login`, { user })
-            .then(res => {
-                console.log(res);
-                console.log(res.data);
-                this.props.setToken(res.data);
-            });
-    }
-
-    render() {
-        return (
-            <div className="auth-wrapper">
-                <div className="auth-inner">
-                    <form onSubmit={this.handleSubmit}>
-                        <h3>Sign In</h3>
-
-                        <div className="form-group">
-                            <label>Username</label>
-                            <input name="username" type="text" className="form-control" onChange={this.handleChange} placeholder="Enter Username" />
-                        </div>
-
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input name="password" type="password" className="form-control" onChange={this.handleChange} placeholder="Enter password" />
-                        </div>
-
-                        <button type="submit" className="btn btn-primary btn-block">Submit</button>
-
-                    </form>
-                </div>
-            </div>
-        );
-    }
+    return await response.data;
 }
+
+
+export default function Login({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
+
+    return (
+        <div className="auth-wrapper">
+            <div className="auth-inner">
+                <form onSubmit={handleSubmit}>
+                    <h3>Sign In</h3>
+
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input name="username" type="text" className="form-control" onChange={e => setUserName(e.target.value)} placeholder="Enter Username" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input name="password" type="password" className="form-control" onChange={e => setPassword(e.target.value)} placeholder="Enter password" />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+
+                </form>
+            </div>
+        </div>
+    );
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+};
